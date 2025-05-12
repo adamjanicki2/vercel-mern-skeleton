e96499b18513460868b4186f3bc3d18197bfac97
Adam Janicki <adamjanicki2@gmail.com>
2025-05-11 17:52:19 -0700

    move to vite and other cleanup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router";
 import Alert from "src/components/Alert";
 import Footer from "src/components/Footer";
 import Nav from "src/components/Nav";
